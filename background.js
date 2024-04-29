1d692aba1506ebb2f7debfa068e8db9ce4c578bc
husamahmud <husamahmud@gmail.com>
2024-04-29 21:16:40 +0300

    [fix]: Select task titles correctly

--- background.js
+++ background.js
@@ -1,30 +1,36 @@
 chrome.action.onClicked.addListener((tab) => {
-	chrome.scripting.executeScript({
-		target: {tabId: tab.id},
-		func: async () => {
-			let taskDivs = document.querySelectorAll('div[id^="task-num-"]');
-			let dirName = document.querySelector('div.list-group-item > ul > li:nth-child(2) > code').innerText;
-			let fileLi, fileName, command, textArea, fileNames = [];
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: async () => {
+      let taskDivs = document.querySelectorAll('div[id^="task-num-"]');
+      let dirName = document.querySelector(
+        "div.list-group-item > ul > li:nth-child(2) > code",
+      ).innerText;
+      let fileLi,
+        fileName,
+        command,
+        textArea,
+        fileNames = [];
 
-			taskDivs.forEach((taskDiv) => {
-				fileLi = Array.from(taskDiv.querySelectorAll('li')).find(function (li) {
-					return li.innerText.includes("File:");
-				});
-			})
+      taskDivs.forEach((taskDiv) => {
+        fileLi = Array.from(taskDiv.querySelectorAll("li")).find(function (li) {
+          return li.innerText.includes("File:");
+        });
 
-			if (fileLi) {
-				fileName = fileLi.querySelector('code').textContent;
-				fileNames.push(`${dirName}/${fileName}`);
-			}
+        if (fileLi) {
+          fileName = fileLi.querySelector("code").textContent;
+          fileNames.push(`${dirName}/${fileName}`);
+        }
+      });
 
-			command = `mkdir -p ${dirName} && touch ${fileNames.join(" ")} && chmod u+x ${dirName}/* && echo "# ${dirName}" > ${dirName}/README.md `;
+      command = `mkdir -p ${dirName} && touch ${fileNames.join(" ")} && chmod u+x ${dirName}/* && echo "# ${dirName}" > ${dirName}/README.md `;
 
-			textArea = document.createElement("textarea");
-			textArea.value = command;
-			document.body.appendChild(textArea);
-			textArea.select();
-			document.execCommand("copy");
-			document.body.removeChild(textArea);
-		}
-	});
+      textArea = document.createElement("textarea");
+      textArea.value = command;
+      document.body.appendChild(textArea);
+      textArea.select();
+      document.execCommand("copy");
+      document.body.removeChild(textArea);
+    },
+  });
 });
